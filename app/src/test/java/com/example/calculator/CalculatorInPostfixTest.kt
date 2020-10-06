487e40c23d2efd49cee2080758feda5ae9214e64
pyaiveoleg <pyaive.oleg@yandex.ru>
2020-10-07 01:05:07 +0300

    fixed Test from junit

--- app/src/test/java/com/example/calculator/CalculatorInPostfixTest.kt
+++ app/src/test/java/com/example/calculator/CalculatorInPostfixTest.kt
@@ -1,6 +1,6 @@
 package com.example.calculator
 
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertEquals
 
